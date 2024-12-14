e6396f5bd054e21e61590838ebde9a7661b3093b
Rohtash Sethi <rohtash.sethi@gmail.com>
2024-12-14 22:16:59 +0530

    updated routes

--- projects/demo/src/app/app.routes.ts
+++ projects/demo/src/app/app.routes.ts
@@ -1,14 +1,9 @@
 import { Routes } from '@angular/router';
 import { DemoComponent } from './demo/demo.component';
-import { BrandingComponent } from './branding/branding.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: BrandingComponent
-    },
-    {
-        path: 'demo',
         component: DemoComponent
     }
 ];
