1c27a683511a7acc2d0baec622895c31be9c2cb5
Rohtash Sethi <rohtash.sethi@gmail.com>
2024-12-14 17:37:24 +0530

    3: Generate demo app

---
+++ projects/demo/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = "It's Rohtash Sethi 👨‍💻🥷🌟";
+}
